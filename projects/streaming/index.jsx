ee2d07fb690b0ae7f14ffc941ce4c70d35edf139
Alek <alek11221@gmail.com>
2024-12-04 13:54:11 -0500

    removed livestream heatmap to seperate repo

--- projects/streaming/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import Dashboard from './dashboard';
-
-const root = createRoot(document.getElementById('root'));
-root.render(<Dashboard />);
